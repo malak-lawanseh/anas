bcaeacff5a96dd5712a38ac949df88c321779da8
malak-lawanseh <malaklawanseh0@gmail.com>
2022-02-07 22:17:08 +0200

    fixing some buges in menu bar

--- app.js
+++ app.js
@@ -1,36 +1,65 @@
 document.addEventListener("DOMContentLoaded",()=>{
+let menuBar=document.querySelector('#menu_bar')
 let content =document.querySelector('.content')
-document.querySelector('#about').onclick=function(){
+
+document.querySelector('.about').onclick=function(){
     content.innerHTML="<div id='aboutme'><img src='img/anas.jpg' ><div id='word'><h1>About Me</h1> <br> <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et, nihil.</p></div></div>"   
+    
+    menuBar.style.visibility= '';
+
 }
-document.querySelector('#home').onclick=function(){
+document.querySelector('.home').onclick=function(){
     content.innerHTML="<div class='content'><div class='hero'><h1 >Anas Lawanseh</h1><br><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt molestias, quos modi iure sapiente fugit nam animi fugiat vero maiores.</p></div><img src='img/undraw_Teaching_re_g7e3.png' class='animation'></div></div>"
+    menuBar.style.visibility= '';
+
 }
-document.querySelector('#skill').onclick=function(){
+document.querySelector('.skill').onclick=function(){
     content.innerHTML="<div class='skillss'><div class='skills'><h1 >Skills</h1><br><ul><li>Lorem, ipsum.</li><li>Lorem, ipsum.</li><li>Lorem, ipsum.</li><li>Lorem, ipsum.</li></ul></div><img src='img/undraw_Personal_goals_re_iow7.png' class='animation' ></div></div>"
+    menuBar.style.visibility= '';
+
 }
-document.querySelector('#Project').onclick=function(){
-    content.innerHTML="<div class='all_cont_in_proj'> <div id='aboutme'> <img src='img/anas.jpg'> <div id='word'> <h1>About Me</h1> <br> <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et, nihil.</p> </div> </div> <div class='content'> <div class='hero'> <h1>Anas Lawanseh</h1> <br> <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt molestias, quos modi iure sapiente fugit nam animi fugiat vero maiores.</p> </div> <img src='img/undraw_Teaching_re_g7e3.png'> </div> <div class='all_cont_in_proj'> <div id='aboutme'> <img src='img/anas.jpg'> <div id='word'> <h1>About Me</h1> <br> <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et, nihil.</p> </div> </div> <div class='content'> <div class='hero'> <h1>Anas Lawanseh</h1> <br> <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt molestias, quos modi iure sapiente fugit nam animi fugiat vero maiores.</p> </div> <img src='img/undraw_Teaching_re_g7e3.png'> </div> </div> <div class='all_cont_in_proj'> <div id='aboutme'> <img src='img/anas.jpg' style='margen-top=10px'> <div id='word'> <h1>About Me</h1> <br> <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et, nihil.</p> </div> </div> </div></div> "
+document.querySelector('.Project').onclick=function(){
+    content.innerHTML="<div class='all_cont_in_proj'> <div id='aboutme'> <img src='img/anas.jpg'> <div id='word'> <h1>About Me</h1> <br> <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et, nihil.</p> </div> </div> <div class='content'> <img src='img/undraw_Teaching_re_g7e3.png'> <div class='hero'> <h1>Anas Lawanseh</h1> <br> <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt molestias, quos modi iure sapiente fugit nam animi fugiat vero maiores.</p> </div> </div> <div class='all_cont_in_proj'> <div id='aboutme'> <img src='img/anas.jpg'> <div id='word'> <h1>About Me</h1> <br> <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et, nihil.</p> </div> </div> <div class='content'> <img src='img/undraw_Teaching_re_g7e3.png'> <div class='hero'> <h1>Anas Lawanseh</h1> <br> <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt molestias, quos modi iure sapiente fugit nam animi fugiat vero maiores.</p> </div> </div> </div> <div class='all_cont_in_proj'> <div id='aboutme'> <img src='img/anas.jpg' style='margen-top=10px'> <div id='word'> <h1>About Me</h1> <br> <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et, nihil.</p> </div> </div> </div> </div>";
+    menuBar.style.visibility= '';
+    
 }
 
+
+document.querySelector('.list1 .about').onclick=function(){
+    content.innerHTML="<div id='aboutme'><img src='img/anas.jpg' ><div id='word'><h1>About Me</h1> <br> <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et, nihil.</p></div></div>"   
+    
+    menuBar.style.visibility= '';
+
+}
+document.querySelector('.list1 .home').onclick=function(){
+    content.innerHTML="<div class='content'><div class='hero'><h1 >Anas Lawanseh</h1><br><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt molestias, quos modi iure sapiente fugit nam animi fugiat vero maiores.</p></div><img src='img/undraw_Teaching_re_g7e3.png' class='animation'></div></div>"
+    menuBar.style.visibility= '';
+
+}
+document.querySelector('.list1 .skill').onclick=function(){
+    content.innerHTML="<div class='skillss'><div class='skills'><h1 >Skills</h1><br><ul><li>Lorem, ipsum.</li><li>Lorem, ipsum.</li><li>Lorem, ipsum.</li><li>Lorem, ipsum.</li></ul></div><img src='img/undraw_Personal_goals_re_iow7.png' class='animation' ></div></div>"
+    menuBar.style.visibility= '';
+
+}
+document.querySelector('.list1  .Project').onclick=function(){
+    content.innerHTML="<div class='all_cont_in_proj'> <div id='aboutme'> <img src='img/anas.jpg'> <div id='word'> <h1>About Me</h1> <br> <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et, nihil.</p> </div> </div> <div class='content'> <img src='img/undraw_Teaching_re_g7e3.png'> <div class='hero'> <h1>Anas Lawanseh</h1> <br> <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt molestias, quos modi iure sapiente fugit nam animi fugiat vero maiores.</p> </div> </div> <div class='all_cont_in_proj'> <div id='aboutme'> <img src='img/anas.jpg'> <div id='word'> <h1>About Me</h1> <br> <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et, nihil.</p> </div> </div> <div class='content'> <img src='img/undraw_Teaching_re_g7e3.png'> <div class='hero'> <h1>Anas Lawanseh</h1> <br> <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt molestias, quos modi iure sapiente fugit nam animi fugiat vero maiores.</p> </div> </div> </div> <div class='all_cont_in_proj'> <div id='aboutme'> <img src='img/anas.jpg' style='margen-top=10px'> <div id='word'> <h1>About Me</h1> <br> <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et, nihil.</p> </div> </div> </div> </div>";
+    menuBar.style.visibility= '';
+    
+}
 
 
 })
-let ul=document.querySelector('.list')
-function myFunction(x) {
-    if (x.matches) { // If media query matches
-      document.querySelector('.list').innerHTML = "<i class='fas fa-bars'  id ='menu'></i>";
-        ul.className='meenu'
-        let menu=document.querySelector('#menu')
-        menu.addEventListener('onclick',()=>{
-    
-            document.getElementById("menu_bar").style.visibility = "visible ";
-        })}
-    else {
-        document.querySelector('.list').innerHTML =' <li><a href="#" id="home">Home</a></li><li><a href="#" id="about">About Me</a></li><li><a href="#" id="skill">Skills</a></li><li><a href="#" id="Project">Project</a></li><a href="https://www.linkedin.com/in/anaslawansah/" target="blank" class="btn">Contact </a>'
-        ul.className='list'
+
+
+
+document.querySelector('#menu').onclick=function(){
+    let menuBar=document.querySelector('#menu_bar')
+        if(menuBar.style.visibility==''){
+            menuBar.style.visibility= 'visible';
     }
-  }
-let x = window.matchMedia("(max-width: 800px)")
-myFunction(x)
-x.addEventListener('media',myFunction)
+    else if(menuBar.style.visibility=='visible'){
+        menuBar.style.visibility= '';
+    }
+ 
+}
+
